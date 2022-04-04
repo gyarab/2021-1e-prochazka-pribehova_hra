716a11c54446fa44f85f153b564f4271f5d32b55
VitezslavProchazka <95168347+VitezslavProchazka@users.noreply.github.com>
2022-04-04 18:50:16 +0200

    Delete module-info.java

--- rocnikovaprace/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.rocnikovaprace {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.ikonli.javafx;
-
-    opens com.example.rocnikovaprace to javafx.fxml;
-    exports com.example.rocnikovaprace;
-}
